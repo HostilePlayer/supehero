9f180f1549e98c30c9b893e96a54f9dabe4c0bae
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-12 15:39:41 +0200

    add all values

---
+++ src/main/java/dk/kea/Main.java
@@ -0,0 +1,40 @@
+package dk.kea;
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+
+    String superHeroName;
+    String realName;
+    boolean isHuman;
+    int yearOfCreation;
+    int Strength;
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Whelcome to the Superhero Database!");
+        System.out.println("please add superhero");
+
+        Main Program = new Main();
+        Program.start();
+
+    }
+
+    public void start(){
+
+        System.out.println("Write a the superhero's name");
+        superHeroName = scanner.next();
+        System.out.println("Write the superhero's real name");
+        realName = scanner.next();
+        System.out.println("Is the Superhero human? true or false");
+        isHuman = scanner.hasNext();
+        System.out.println("What year whas the hero first written about?");
+        yearOfCreation = scanner.nextInt();
+        System.out.println("how much force in Newton can the superhero do?");
+        Strength = scanner.nextInt();
+
+    }
+
+}
